a830be09835ad663fe8618ff6a818f111586878b
Emre <emrkara1999@gmail.com>
2024-02-17 23:24:16 +0300

    Main Function added.

--- src/deneme.kt
+++ src/deneme.kt
@@ -1,9 +1,14 @@
-fun main()  {
+fun main() {
+    println("Welcome")
+    val calculator = Calculator()
+    calculator.calculate()
+    calculator.continueOrNot()
 
 }
-class Calculator (firstNumber: Float, secondNumber: Float, operator: String){
-    val firstNo : Float?
-    val secondNo : Float?
+
+class Calculator {
+    val firstNo: Float?
+    val secondNo: Float?
 
     init {
         println("Enter First Number")
@@ -11,38 +16,49 @@ class Calculator (firstNumber: Float, secondNumber: Float, operator: String){
         println("Enter Second Number")
         secondNo = readln()?.toFloatOrNull()
     }
-    fun calculate (firstNumber : Float , secondNumber : Float, operator : String){
+
+    fun calculate() {
         println("Enter the operator + - / *")
         val operator = readln()
-
-        when(operator){
+        when (operator) {
             "+" -> println("Result : ${sum()}")
             "-" -> println("Result : ${minus()}")
             "*" -> println("Result : ${times()}")
             "/" -> println("Result : ${divide()}")
-
-            }
         }
+    }
 
-
-    private fun sum() : Float{
+    private fun sum(): Float {
         return firstNo!! + secondNo!!
 
     }
-    private fun minus() : Float{
-      return  firstNo!! - secondNo!!
-    }
 
-    private fun divide() : Float{
-        return if (secondNo?.toInt() != 0){
+    private fun minus(): Float {
+        return firstNo!! - secondNo!!
+    }
+    private fun divide(): Float {
+        return if (secondNo?.toInt() != 0) {
             firstNo!! / secondNo!!
-        }else{
+        } else {
             println("Cannot Divide By Zer0")
             0f
         }
     }
-    private fun times() : Float {
+    private fun times(): Float {
         return firstNo!! * secondNo!!
+    }
+
+
+     fun continueOrNot(): Any {
+        val input :String = readln()
+        return when(input){
+            "Y" -> Calculator()
+            else -> {
+                println("Try Again")
+                Calculator()
+            }
+        }
+
     }
 
 }
